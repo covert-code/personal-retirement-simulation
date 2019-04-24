4632451643f5811d0a66637a8ed96da898b24e02
Allen Jiang <allen.jiang@outlook.com>
2019-04-24 02:26:30 -0500

    Participant data model

--- src/app/models/IParticipant.ts
+++ src/app/models/IParticipant.ts
@@ -1,8 +1,14 @@
 export interface IParticipant {
   user_id: number;
-  participant_name_first: string;
-  participant_name_middle: string;
-  participant_name_last: string;
+  user_name: string;
+  participant_addr_1: string;
+  participant_addr_2: string;
+  participant_addr_city: string;
+  participant_addr_state: string;
+  participant_addr_zip: string;
+  participant_birthdate: string;
+  participant_income: number;
+  participant_marital: string;
 }
 
 
