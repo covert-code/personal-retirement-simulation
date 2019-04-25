670522892879b58b8821a6de19b5d489a4dd52eb
Allen Jiang <allen.jiang@outlook.com>
2019-04-24 20:39:48 -0500

    Added empty footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,35 @@
+// Basic Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+// Routing Module
 import { AppRoutingModule } from './app-routing.module';
+// Main App
 import { AppComponent } from './app.component';
+// Heder, Footer
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+// Forms (and Dependencies)
+import { InputService } from './services/input.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IndexJumbotronComponent } from './index.jumbotron/index.jumbotron.component';
 import { SurveyFormComponent } from './survey.form/survey.form.component';
 import { ParticipantdataFormComponent } from './participantdata.form/participantdata.form.component';
-import { InputService } from './services/input.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IndexComponent } from './index/index.component';
+import { Index } from './index.cards/index.cards.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
     IndexJumbotronComponent,
     SurveyFormComponent,
-    ParticipantdataFormComponent
+    ParticipantdataFormComponent,
+    IndexComponent,
+    Index.CardsComponent
   ],
   imports: [
     BrowserModule,
@@ -34,9 +44,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   bootstrap: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
     IndexJumbotronComponent,
     SurveyFormComponent,
-    ParticipantdataFormComponent
+    ParticipantdataFormComponent,
   ]
 })
 export class AppModule { }
