d70c287391b9a346ac31153c9ad6f9d6fd7a0472
Allen Jiang <allen.jiang@outlook.com>
2019-04-30 03:45:57 -0500

    indentation fix

--- express/routes/routes-user-creation.js
+++ express/routes/routes-user-creation.js
@@ -6,13 +6,13 @@ module.exports = ((app) => {
   });
 
   app.post('/postParticipantData', (req, res) => {
-      // write to db
-      res.status(204).end();
+    // write to db
+    res.status(204).end();
   });
 
   app.post('/postParticipantSurveyData', (req, res) => {
-      // write to db
-      res.status(204).end();
+    // write to db
+    res.status(204).end();
   });
 
 });
