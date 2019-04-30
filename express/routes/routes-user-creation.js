f720c0cf2d3a4123cea4ad7923972079500cf2b7
Allen Jiang <allen.jiang@outlook.com>
2019-04-30 07:30:23 -0500

    db connection endpoint check for user-exists stored proc

--- express/routes/routes-user-creation.js
+++ express/routes/routes-user-creation.js
@@ -1,4 +1,25 @@
+var mysql = require('mysql');
+var dbc = require('../modules/db-connect');
+
 module.exports = ((app) => {
+
+  app.post('/getUserExists', (req, res) => {
+    var email = req.body.user_email;
+
+    var con = dbc.open(); if (con == null) { return; }
+
+    dbc.stored(con, 'exists_user', ['jane.doe@gmail.com'],
+      (results, fields) => {
+        if (results[0][0][fields[0][0].name] == 0) {
+          res.json({'user_email': email, 'exists': true});
+        } else {
+          res.json({'user_email': email, 'exists': false});
+        }
+      }
+    );
+
+    con.end();
+  })
 
   app.post('/postCreateUser', (req, res) => {
     // write to db
