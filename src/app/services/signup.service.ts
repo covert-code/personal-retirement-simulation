7363f26273b0b5a1e78b5bbcf9b136fd6e28fb7d
Allen Jiang <allen.jiang@outlook.com>
2019-04-29 19:24:36 -0500

    annotations for signupservice

--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -2,17 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+// Models
 import { IParticipant } from 'src/app/models/IParticipant';
 import { IParticipantSurvey } from 'src/app/models/IParticipantSurvey';
 
-const expressPort = 3000;
+/*
+SignupService
+
+This service contains supporting functions related to HTTP communications to server
+for pages involved in the creation of new user accounts and initial survey details.
+*/
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
+
   constructor(private http: HttpClient) { }
 
+  /* participantdata.form */
   postParticipantData(data: IParticipant): Observable<IParticipant> {
     return this.http.post<IParticipant>(
     	'http://localhost:3000/postParticipantData',
@@ -20,6 +28,8 @@ export class SignupService {
     );
   }
 
+
+  /* survey.form */
   postParticipantSurveyData(data: IParticipantSurvey): Observable<IParticipantSurvey> {
     return this.http.post<IParticipantSurvey>(
       'http://localhost:3000/postParticipantSurveyData',
