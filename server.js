3833f61315b064c9aed14a8ec1359561b3d9a612
Allen Jiang <allen.jiang@outlook.com>
2019-04-30 00:39:42 -0500

    sectioning and body-parser

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
+/* Setup
+------------------
+Basic package and config loading
+*/
+
 // Config Files
-const config = require('./config/server.json')
+const config = require('./config/server.json');
 
 // Express
-const express = require('express')
-const port = config.express_port
+const express = require('express');
+const app = express();
+const port = config.express_port;
 
-// Imports
+
+/* Middleware
+------------------
+Middleware imports for Express application
+*/
+
+// BodyParser
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 // Cross-Origin Resource Sharing
-const cors = require('cors')
+const cors = require('cors');
 var corsOptions = {
 	origin: true,
 	optionsSuccessStatus: 200
 }
+app.use(cors(corsOptions));
 
-// Setup
-const app = express()
-app.use(cors(corsOptions))
 
-// Endpoints
+/* Endpoints
+------------------
+Specifications for API Endpoints for this server
+*/
+
 app.get('/', (req, res) => {
 	res.send('Server Running')
 })
@@ -38,7 +54,12 @@ app.post('/postParticipantSurveyData', (req, res) => {
     res.status(204).end();
 })
 
-// Execute
+
+/* Execute
+------------------
+Server application launch point
+*/
+
 app.listen(port, () => console.log(
 	`Personal Retirement Simulation express server listening on port ${port}!`
 ))
