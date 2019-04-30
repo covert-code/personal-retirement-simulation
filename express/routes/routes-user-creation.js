e5283d1221217f087c531aaaf950eaf4a6a29dee
Allen Jiang <allen.jiang@outlook.com>
2019-04-30 03:37:20 -0500

    semicolons

--- express/routes/routes-user-creation.js
+++ express/routes/routes-user-creation.js
@@ -3,16 +3,16 @@ module.exports = ((app) => {
   app.post('/postCreateUser', (req, res) => {
     // write to db
     res.status(204).end();
-  })
+  });
 
   app.post('/postParticipantData', (req, res) => {
       // write to db
       res.status(204).end();
-  })
+  });
 
   app.post('/postParticipantSurveyData', (req, res) => {
       // write to db
       res.status(204).end();
-  })
+  });
 
 });
