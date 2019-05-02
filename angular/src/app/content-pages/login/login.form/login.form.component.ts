3007ebef59a8f4a92120ccb58e9dd4d03c414e7a
Allen Jiang <allen.jiang@outlook.com>
2019-05-01 22:38:36 -0500

    submission to login funcitonality

--- angular/src/app/content-pages/login/login.form/login.form.component.ts
+++ angular/src/app/content-pages/login/login.form/login.form.component.ts
@@ -6,6 +6,7 @@ import { map } from 'rxjs/operators';
 
 import { UserAuthService } from 'src/app/services/userauth.service';
 import {
+  IUserLoginQuery
 } from 'src/app/services/userauth.service.models';
 
 @Component({
@@ -31,6 +32,51 @@ export class LoginFormComponent implements OnInit {
 
   // Main function when submit button is pushed
   onFormSubmitClick(): void {
-    
+    this.validateFormContents().subscribe(
+      (formValid: boolean) => {
+        if (formValid) {
+          this.submitFormContents().subscribe(
+            (submissionSuccess: boolean) => {
+              if (submissionSuccess) {
+                this.navigateNext();
+              }
+            }
+          );
+        }
+      }
+    );
+  }
+
+
+  /* On-Submit Form Validation */
+  loginFormFlags = {
+    authFail: false,
+  }
+
+  validateFormContents(): Observable<boolean> {
+    return of(this.loginForm.valid);
+  }
+
+
+  /* Submit to Server */
+  submitFormContents() : Observable<boolean> {
+    var userLoginQuery: IUserLoginQuery = {
+      user_email: this.loginForm.get('email').value,
+      user_password: this.loginForm.get('password').value,
+    };
+
+    return this.authService.loginUser(userLoginQuery)
+      .pipe(
+        // Post-Reply Behavior
+        map((authSuccess: boolean) => {
+          this.loginFormFlags.authFail = !authSuccess;
+          return authSuccess;
+        })
+      );
+  }
+
+  /* Navigation to Next Route */
+  navigateNext() : void {
+    this.router.navigate(['/about-you']); // todo
   }
 }
