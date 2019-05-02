d2cb4e3d70f844bff096170ede1388947e2f1ad0
Allen Jiang <allen.jiang@outlook.com>
2019-05-02 07:24:52 -0500

    auth state observable

--- angular/src/app/services/userauth.service.models/state.ts
+++ angular/src/app/services/userauth.service.models/state.ts
@@ -1,20 +1,33 @@
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { IAuthenticatedRequestBody } from './protocols';
 
 /*
 Global user state model specification
 */
 export class UserAuthState {
-  authenticated: boolean = false;
+  private authenticated: boolean = false;
+  authenticatedStatus: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.authenticated);
+
   private credentials: ICredentials = {
     username: null,
     password: null,
   }
+
+  constructor() {
+    this.authenticatedStatus.subscribe(
+      (status: boolean) => { this.authenticated = status; }
+    );
+  }
+
+  isAuthenticated(): boolean {
+    return this.authenticated;
+  }
   
   setAuthenticated(username: string, password: string): void {
-    this.authenticated = true;
     this.credentials.username = username;
     this.credentials.password = password;
+
+    this.authenticatedStatus.next(true);
   }
 
   authenticateRequest<T>(request: T): IAuthenticatedRequestBody<T> {
@@ -37,9 +50,10 @@ export class UserAuthState {
   }
 
   deauthenticate(): void {
-    this.authenticated = false;
     this.credentials.username = null;
     this.credentials.password = null;
+
+    this.authenticatedStatus.next(false);
   }
 }
 
