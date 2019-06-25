4293de20902f1db46eb80a204edee24bb5fac0f4
Allen Jiang <allen.jiang@outlook.com>
2019-06-25 15:57:58 -0400

    Common functionality db

---
+++ express/routes/handlers/common/common-db.js
@@ -0,0 +1,38 @@
+const db = require('../../../modules/db/mysql-connect');
+const http_status = require('http-status-codes');
+const env_lib = require('./environment.js');
+
+/* Init and Destroy */
+var prereqs = ['http'];
+
+function env_destroy_db_handler(env) {
+  db.close(env.db.con);
+}
+
+async function init_db_connection(env) {
+  // Attempt DB Connection
+  try {
+    env.db.con = await db.open();
+  }
+  catch (e) {
+    env.http.res
+    .status(http_status.INTERNAL_SERVER_ERROR)
+    .send(
+      { desc:'Unable to connect to database', error: e }
+    );
+  }
+}
+
+module.exports.init_env = async (env) => {
+  env_lib.check_prereqs(env, prereqs);
+  if (env.active) {
+    env.db = {};
+    await init_db_connection(env);
+    env_lib.on_destroy_call(env, env_destroy_db_handler);
+  }
+}
+
+/* Database Stored Procedure Call */
+module.exports.call = (env, stored_proc, params) => {
+  return db.stored(env.db.con, stored_proc, params);
+}
