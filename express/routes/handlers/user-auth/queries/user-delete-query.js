dcb5dc0e929e07994c6754b7738525921c271c0d
Allen Jiang <allen.jiang@outlook.com>
2019-06-25 17:58:37 -0400

    rewrote delete query for common compliance

---
+++ express/routes/handlers/user-auth/queries/user-delete-query.js
@@ -0,0 +1,41 @@
+const db = require('../../common/common-db');
+const http = require('../../common/common-http');
+
+// Calls auth_login on user credentials in env
+// Returns Promise of resolution
+// Sends HTTP on success
+function call_user_delete(env) {
+  // Stored Procedure: user_delete
+  return db.call(
+    env, 'user_delete',
+    [
+      env.auth.user.user_email,
+      env.auth.user.password.hash
+    ]
+  ).then(
+    // query success handling
+    (result) => {
+      if (result.affectedRows > 0) {
+        http.ok(env);
+      } else {
+        http.send(env,
+          http.status.BAD_REQUEST,
+          {desc: 'Unable to delete user'}
+        );
+      }
+    },
+  ).catch(
+    // query error handling
+    (e) => {
+      http.send(env,
+        http.status.INTERNAL_SERVER_ERROR,
+        {
+          desc: 'Unable to generate login',
+          error: { code: e.code, msg: e.sqlMessage }
+        }
+      );
+    }
+  );
+}
+
+module.exports = call_user_delete;
--- express/routes/handlers/user-auth/user-delete.js
+++ express/routes/handlers/user-auth/user-delete.js
@@ -1,101 +1,22 @@
-const db = require('../../../modules/db/mysql-connect');
-const crypto = require('../../../modules/crypto/bcrypt');
-const http_status = require('http-status-codes');
+const common = require('../common/common');
+const auth = require('../common/common-auth');
+
+const populate_login = require('./user-login').populate_login;
+const call_user_delete = require('./queries/user-delete-query');
 
 async function user_delete_handler(req, res) {
-  // Credential Structure
-  var user_credentials = {
-    user_email: req.body.user_email,
-    password: {
-      client_plaintext: req.body.user_password
-    }
-  };
+  // Initialize
+  var env = await common.get_env(req, res);
+  if (env == null || !env.active) { return; }
 
-  // Attempt DB Connection
-  var con = null;
-  try { con = await db.open(); }
-  catch (e) {
-    res.status(http_status.INTERNAL_SERVER_ERROR).send(
-      { desc:'Unable to connect to database', error: e }
-    );
-    return;
-  }
-
-  // Stored Procedure: auth_salt
-  user_credentials.password.salt = await db.stored(
-    con, 'auth_salt',
-    [ user_credentials.user_email ]
-  ).then(
-    // query success handling
-    (result) => {
-      if (result[0].length == 0) {
-        // query result empty
-        res.status(http_status.BAD_REQUEST);
-        res.send({desc: 'Username or password is incorrect'})
-        return null;
-      } else {
-        // query result exists
-        var select_result = result[0][0];
-        return select_result.user_password_salt;
-      }
-    },
-  ).catch(
-    // query error handling
-    (e) => {
-      res.status(http_status.INTERNAL_SERVER_ERROR).send({
-        desc: 'Unable to fetch salt',
-        error: { code: e.code, msg: e.sqlMessage }
-      });
-    }
-  )
-  // Terminate if salt is not found (fatal)
-  if (user_credentials.password.salt == null) { db.close(con); return; }
-
-  
-  // Generate Hash
-  try {
-    user_credentials.password.hash = await crypto.hash_gen_salty(
-      user_credentials.password.client_plaintext,
-      user_credentials.password.salt
-    )
-  }
-  catch (e) {
-    res.status(http_status.INTERNAL_SERVER_ERROR).send(
-      { desc:'Unable to generate password hash', error: e }
-    );
-    return;
-  }
+  // Read and validate login credentials
+  await populate_login(env);
 
   // Stored Procedure: user_delete
-  await db.stored(
-    con, 'user_delete',
-    [
-      user_credentials.user_email,
-      user_credentials.password.hash
-    ]
-  ).then(
-    // query success handling
-    (result) => {
-      if (result.affectedRows > 0) {
-        res.status(http_status.OK).end();
-      } else {
-        res.status(http_status.BAD_REQUEST);
-        res.send({desc: 'Unable to delete user'});
-      }
-    },
-  ).catch(
-    // query error handling
-    (e) => {
-      res.send({
-        desc: 'Unable to generate login',
-        error: { code: e.code, msg: e.sqlMessage }
-      });
-    }
-  )
+  await call_user_delete(env);
 
   // Terminate
-  db.close(con);
-  if (!res.headersSent) { res.status(http_status.NO_CONTENT).end(); }
+  common.end_env(env);
 }
 
 module.exports.handler = user_delete_handler;
