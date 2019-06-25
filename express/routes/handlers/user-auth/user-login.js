8696e03783388173dac31013949d1fe5dea55d13
Allen Jiang <allen.jiang@outlook.com>
2019-06-25 17:43:42 -0400

    Import cleanup

--- express/routes/handlers/user-auth/user-login.js
+++ express/routes/handlers/user-auth/user-login.js
@@ -1,8 +1,5 @@
-const crypto = require('../../../modules/crypto/bcrypt');
-
 const common = require('../common/common');
 const auth = require('../common/common-auth');
-const http = require('../common/common-http');
 
 const call_auth_salt = require('./queries/user-salt-query');
 const call_auth_login = require('./queries/user-login-query');
