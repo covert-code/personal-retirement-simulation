2ff09984cd6af35cc486621eb1fbed33c560ff0c
Allen Jiang <allen.jiang@outlook.com>
2019-06-25 13:42:21 -0400

    added delete to routes

--- express/routes/routes-user-auth.js
+++ express/routes/routes-user-auth.js
@@ -1,10 +1,13 @@
 module.exports = ((app) => {
   /* Endpoint to check the validity of an email/password combination. */
-  app.post('/api/auth/login', require('./handlers/user-auth/user-login'));
+  app.post('/api/auth/login', require('./handlers/user-auth/user-login').handler);
 
   /* Endpoint to check whether a user exists or not. */
-  app.post('/api/auth/logout', require('./handlers/user-auth/user-logout'));
+  app.post('/api/auth/logout', require('./handlers/user-auth/user-logout').handler);
 
   /* Endpoint to register a new user. */
-  app.post('/api/auth/register/new', require('./handlers/user-auth/user-create'));
+  app.post('/api/auth/register/new', require('./handlers/user-auth/user-create').handler);
+
+  /* Endpoint to delete a user */
+  app.post('/api/auth/delete', require('./handlers/user-auth/user-delete').handler);
 });
