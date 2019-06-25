40922890503a1698788b5100be2c59b0461564b8
Allen Jiang <allen.jiang@outlook.com>
2019-06-25 17:41:07 -0400

    Login rewritten for common compliance and added exportable do_login

--- express/routes/handlers/user-auth/user-login.js
+++ express/routes/handlers/user-auth/user-login.js
@@ -12,29 +12,8 @@ async function user_login_handler(req, res) {
   var env = await common.get_env(req, res);
   if (env == null || !env.active) { return; }
 
-  // Get user credentials from request
-  auth.req_read_user_auth(env);
-
-  // Stored Procedure: auth_salt
-  env.auth.user.password.salt = await call_auth_salt(env);
-  // Terminate if salt is not found (fatal)
-  if (env.auth.user.password.salt == null) { common.end_env(env); return; }
-
-  
-  // Generate Hash
-  try {
-    env.auth.user.password.hash = await crypto.hash_gen_salty(
-      env.auth.user.password.plaintext,
-      env.auth.user.password.salt
-    )
-  }
-  catch (e) {
-    res.status(http.status.INTERNAL_SERVER_ERROR).send(
-      { desc:'Unable to generate password hash', error: e }
-    );
-    return;
-  }
-
+  // Read and validate login credentials
+  await populate_login(env);
 
   // Stored Procedure: auth_login
   await call_auth_login(env);
@@ -44,3 +23,18 @@ async function user_login_handler(req, res) {
 }
 
 module.exports.handler = user_login_handler;
+
+async function populate_login(env) {
+  // Get user credentials from request
+  auth.req_read_user_auth(env);
+
+  // Stored Procedure: auth_salt
+  env.auth.user.password.salt = await call_auth_salt(env);
+  // Terminate if salt is not found (fatal)
+  if (env.auth.user.password.salt == null) { common.end_env(env); return; }
+
+  // Generate password hash
+  if (await auth.hash_salty(env) == null) { common.end_env(env); return; }
+}
+
+module.exports.do_login = populate_login;
