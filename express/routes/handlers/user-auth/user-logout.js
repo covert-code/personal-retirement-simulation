ba8840f6f5877ee1c1f2dc02ee8f9c6051eea405
Allen Jiang <allen.jiang@outlook.com>
2019-06-25 02:34:01 -0400

    user logout endpoint

---
+++ express/routes/handlers/user-auth/user-logout.js
@@ -0,0 +1,56 @@
+const db = require('../../../modules/db/mysql-connect');
+const crypto = require('../../../modules/crypto/bcrypt');
+const http_status = require('http-status-codes');
+
+async function user_logout_handler(req, res) {
+  // Credential Structure
+  var user_credentials = {
+    client: {
+      id: req.body.client.id,
+      auth: req.body.client.auth
+    }
+  };
+
+  // Attempt DB Connection
+  var con = null;
+  try { con = await db.open(); }
+  catch (e) {
+    res.status(http_status.INTERNAL_SERVER_ERROR).send(
+      { desc:'Unable to connect to database', error: e }
+    );
+    return;
+  }
+
+  // Stored Procedure: auth_logout
+  await db.stored(
+    con, 'auth_logout',
+    [
+      user_credentials.client.id,
+      user_credentials.client.auth
+    ]
+  ).then(
+    // query success handling
+    (result) => {
+      if (result.affectedRows > 0) {
+        res.status(http_status.OK).end();
+      } else {
+        res.status(http_status.BAD_REQUEST);
+        res.send({desc: 'Bad or expired client credentials'});
+      }
+    },
+  ).catch(
+    // query error handling
+    (e) => {
+      res.send({
+        desc: 'Unable to safely logout client',
+        error: { code: e.code, msg: e.sqlMessage }
+      });
+    }
+  )
+
+  // Terminate
+  db.close(con);
+  if (!res.headersSent) { res.status(http_status.NO_CONTENT).end(); }
+}
+
+module.exports = user_logout_handler;
--- express/routes/routes-user-auth.js
+++ express/routes/routes-user-auth.js
@@ -3,9 +3,7 @@ module.exports = ((app) => {
   app.post('/api/auth/login', require('./handlers/user-auth/user-login'));
 
   /* Endpoint to check whether a user exists or not. */
-  app.post('/api/auth/register/taken', (req, res) => {
-    res.status(204).end();
-  });
+  app.post('/api/auth/logout', require('./handlers/user-auth/user-logout'));
 
   /* Endpoint to register a new user. */
   app.post('/api/auth/register/new', require('./handlers/user-auth/user-create'));
