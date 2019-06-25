0891492ef5e5049abd22d2de7031bc78c76d970d
Allen Jiang <allen.jiang@outlook.com>
2019-06-25 17:14:55 -0400

    logout query file

---
+++ express/routes/handlers/user-auth/user-logout-query.js
@@ -0,0 +1,40 @@
+const db = require('../common/common-db');
+const http = require('../common/common-http');
+
+// Calls auth_logout on client credentials in env
+// Returns Promise of resolution
+function call_user_logout(env) {
+  // Stored Procedure: auth_logout
+  return db.call(env,
+    'auth_logout',
+    [
+      env.auth.client.id,
+      env.auth.client.authcode
+    ]
+  ).then(
+    // query success handling
+    (result) => {
+      if (result.affectedRows > 0) {
+        http.ok();
+      } else {
+        http.send(env,
+          http.status.BAD_REQUEST,
+          {desc: 'Bad or expired client credentials'}
+        );
+      }
+    },
+  ).catch(
+    // query error handling
+    (e) => {
+      http.send(env,
+        http.status.INTERNAL_SERVER_ERROR,
+        {
+          desc: 'Unable to safely logout client',
+          error: { code: e.code, msg: e.sqlMessage }
+        }
+      );
+    }
+  );
+}
+
+module.exports = call_user_logout;
