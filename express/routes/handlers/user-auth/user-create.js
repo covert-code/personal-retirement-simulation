e6509233ed41b761b6724a0c27a7776c4e08d408
Allen Jiang <allen.jiang@outlook.com>
2019-06-25 19:19:05 -0400

    rewritten user-create handler for common

--- express/routes/handlers/user-auth/user-create.js
+++ express/routes/handlers/user-auth/user-create.js
@@ -1,102 +1,31 @@
-const db = require('../../../modules/db/mysql-connect');
-const crypto = require('../../../modules/crypto/bcrypt');
-const http_status = require('http-status-codes');
+const common = require('../common/common');
+const auth = require('../common/common-auth');
+
+const call_auth_login = require('./queries/user-login-query');
+const call_user_create = require('./queries/user-create-query');
 
 async function user_create_handler(req, res) {
-  // Generate Hash
-  var user_pw_hash = null;
-  try { user_pw_hash = await crypto.hash_gen(req.body.user_password) }
-  catch (e) {
-    res.status(http_status.INTERNAL_SERVER_ERROR).send(
-      { desc:'Unable to generate password hash', error: e }
-    );
-    return;
-  }
+  // Initialize
+  var env = await common.get_env(req, res);
+  if (env == null || !env.active) { return; }
 
-  // Formalize Query Data
-  var query_data = {
-    /* Credentials */
-    user_email: req.body.user_email,
-    user_password: user_pw_hash,
-    /* Name */
-    user_name: [
-      req.body.user_name_title,
-      req.body.user_name_first,
-      req.body.user_name_initial,
-      req.body.user_name_last
-    ]
-  };
+  // Get user credentials and name from request
+  auth.req_read_user_auth(env);
+  auth.req_read_user_name(env);
 
-  // Attempt DB Connection
-  var con = null;
-  try { con = await db.open(); }
-  catch (e) {
-    res.status(http_status.INTERNAL_SERVER_ERROR).send(
-      { desc:'Unable to connect to database', error: e }
-    );
-    return;
-  }
+  // Generate hash
+  await auth.hash(env);
 
   // Stored Procedure: user_create
-  var user_create_success = await db.stored(
-    con, 'user_create',
-    [
-      query_data.user_email,
-      query_data.user_password,
-      ...query_data.user_name // unpacking
-    ],
-  ).then(
-    // query success handling
-    (result) => { return true; },
-  ).catch(
-    // query error handling
-    (e) => {
-      var err_desc;
-      if (e.code == 'ER_DUP_ENTRY') {
-        res.status(http_status.BAD_REQUEST);
-        err_desc = 'Email is already registered';
-      } else {
-        res.status(http_status.INTERNAL_SERVER_ERROR);
-        err_desc = 'Unable to create new user';
-      }
-      res.send({desc: err_desc, error: { code: e.code, msg: e.sqlMessage }});
-      return false;
-    }
-  );
-  
+  var user_create_success = await call_user_create(env);
+
   // Stored Procedure: auth_login
   if (user_create_success) {
-    await db.stored(
-      con, 'auth_login',
-      [
-        query_data.user_email,
-        query_data.user_password
-      ]
-    ).then(
-      // query success handling
-      (result) => {
-        var select_result = result[0][0];
-        res.status(http_status.OK).send({
-          client: {
-            id: select_result.token_client_id,
-            auth: select_result.token_auth_code
-          }
-        })
-      },
-    ).catch(
-      // query error handling
-      (e) => {
-        res.send({
-          desc: 'Unable to generate login',
-          error: { code: e.code, msg: e.sqlMessage }
-        });
-      }
-    )
+    await call_auth_login(env);
   }
 
   // Terminate
-  db.close(con);
-  if (!res.headersSent) { res.status(http_status.NO_CONTENT).end(); }
+  common.end_env(env);
 }
 
 module.exports.handler = user_create_handler;
