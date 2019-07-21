f2792dde9ac5925bf8be73758d441062bd38ab3d
Allen Jiang <allen.jiang@outlook.com>
2019-07-21 13:33:06 -0400

    read return body formatting

--- express/routes/handlers/survey/queries/survey_pd_read_query.js
+++ express/routes/handlers/survey/queries/survey_pd_read_query.js
@@ -21,7 +21,21 @@ function call_survey_pd_read(env) {
       var body = { survey_pd: null }
 
       if (row.length > 0) {
-        body.survey_pd = row[0];
+        var data = row[0];
+        // construct response body
+        body.survey_pd = {
+          preseed: data.survey_pd_preseed[0] == 1, // convert bit buffer to bool
+          address: {
+            street_1: data.survey_pd_address_1,
+            street_2: data.survey_pd_address_2,
+            city: data.survey_pd_addr_city,
+            state: data.survey_pd_addr_state,
+            zip: data.survey_pd_addr_zip
+          },
+          birthdate: data.survey_pd_birthdate,
+          income: data.survey_pd_income,
+          marital: data.survey_pd_marital
+        }
       }
 
       // Reply
