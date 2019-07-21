9b23afa1d4713fa64b0d0b34ff7ed720a511c170
Allen Jiang <allen.jiang@outlook.com>
2019-07-21 13:34:22 -0400

    rt survey endpoints added to routes-surveys

--- express/routes/routes-surveys.js
+++ express/routes/routes-surveys.js
@@ -2,6 +2,12 @@ module.exports = ((app) => {
   /* Endpoint to fetch the user participant data survey */
   app.post('/api/surveys/pd/read', require('./handlers/survey/user-pd-read').handler);
 
+  /* Endpoint to write the user participant data survey */
+  app.post('/api/surveys/pd/write', require('./handlers/survey/user-pd-write').handler);
+
   /* Endpoint to fetch the user retirement survey */
   app.post('/api/surveys/rt/read', require('./handlers/survey/user-rt-read').handler);
+
+  /* Endpoint to write the user participant data survey */
+  app.post('/api/surveys/rt/write', require('./handlers/survey/user-rt-write').handler);
 });
