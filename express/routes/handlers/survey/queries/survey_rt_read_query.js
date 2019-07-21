4cf89b95fef18e35ff97c6f08333082371c335a9
Allen Jiang <allen.jiang@outlook.com>
2019-07-21 13:33:46 -0400

    rt read query

--- express/routes/handlers/survey/queries/survey_rt_read_query.js
+++ express/routes/handlers/survey/queries/survey_rt_read_query.js
@@ -21,7 +21,22 @@ function call_survey_rt_read(env) {
       var body = { survey_rt: null }
 
       if (row.length > 0) {
-        body.survey_rt = row[0];
+        var data = row[0];
+        // construct response body
+        body.survey_rt = {
+          preseed: data.survey_rt_preseed[0] == 1, // convert bit buffer to bool
+          goal: {
+            age: data.survey_rt_age,
+            goal: data.survey_rt_goal
+          },
+          current_savings: data.survey_rt_curr_savings,
+          employer_deposit: data.survey_rt_employer_deposit,
+          birthdate: data.survey_pd_birthdate,
+          concerns: {
+            lifetime: data.survey_rt_lifetime_concern[0] == 1,
+            social_security: data.survey_rt_ss[0] == 1
+          }
+        }
       }
 
       // Reply
